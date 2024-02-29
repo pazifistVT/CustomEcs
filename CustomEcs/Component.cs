c45a107042551ff83b2f41ea07219aaac2e84212
pazifistVT <FenixVT@yandex.ru>
2024-02-29 20:22:39 +0500

    Мелкие исправлния

--- CustomEcs/Component.cs
+++ CustomEcs/Component.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization.Formatters;
 using System.Text.Json;
 
 namespace CustomEcs
--- CustomEcs/Entity.cs
+++ CustomEcs/Entity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics.Eventing.Reader;
 using System.Text.Json;
 namespace CustomEcs
 {
